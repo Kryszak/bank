5a5b7aa0bbaac0408f7ec4ee7136f0d656ad2a84
Kryszak <prajskrzysztof@gmail.com>
2018-01-05 14:15:25 +0100

    podrasowanie klienta rest

--- Server/src/main/java/bsr/project/bank/webservice/external/ExternalBankClient.java
+++ Server/src/main/java/bsr/project/bank/webservice/external/ExternalBankClient.java
@@ -6,11 +6,9 @@ import bsr.project.bank.utility.logging.LogMethodCall;
 import bsr.project.bank.webservice.external.validation.ValidationError;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -19,7 +17,6 @@ import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
-import java.nio.charset.Charset;
 
 @Slf4j
 @Service
@@ -30,16 +27,27 @@ public class ExternalBankClient {
 
     @Value("${app.config.externalBankUri}")
     private String uri;
-    
+
     @Value("${app.config.externalAuth.user}")
     private String authUser;
 
     @Value("${app.config.externalAuth.password}")
     private String authPassword;
 
+    @Value("${app.config.client.readTimeout}")
+    private int readTimeout;
+
+    @Value("${app.config.client.connectTimeout}")
+    private int connectTimeout;
+
     @LogMethodCall
     public void requestExternalTransfer(ExternalTransfer transfer, String destinationAccount) throws IOException {
-        RestTemplate restTemplate = new RestTemplate();
+        RestTemplateBuilder builder = new RestTemplateBuilder();
+        RestTemplate restTemplate = builder
+                .basicAuthorization(authUser, authPassword)
+                .setConnectTimeout(connectTimeout)
+                .setReadTimeout(readTimeout)
+                .build();
 
         String bankCode = destinationAccount.substring(2, 10);
         String bankUrl = bankListService.getBankUrl(bankCode);
@@ -50,9 +58,9 @@ public class ExternalBankClient {
         try {
             ResponseEntity<Void> response = restTemplate.postForEntity(
                     url,
-                    new HttpEntity<>(transfer, createHeaders(authUser, authPassword)),
+                    transfer,
                     Void.class);
-
+            log.info("Request complete with status {}", response.getStatusCode());
         } catch (HttpClientErrorException ex) {
             HttpStatus status = ex.getStatusCode();
             log.info("Request failed with status {}: {}", status, ex.getMessage());
@@ -75,13 +83,4 @@ public class ExternalBankClient {
 
     }
 
-    private HttpHeaders createHeaders(String username, String password) {
-        HttpHeaders headers = new HttpHeaders();
-        String auth = username + ":" + password;
-        byte[] encodedAuth = Base64.encodeBase64(
-                auth.getBytes(Charset.forName("US-ASCII")));
-        String authHeader = "Basic " + new String(encodedAuth);
-        headers.set("Authorization", authHeader);
-        return headers;
-    }
 }
